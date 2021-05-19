6726a1a4aa7b391977382e0e0d6c9f6f81025c13
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2021-05-19 13:33:44 -0700

    javascript started

---
+++ helloworld website/page_script.js
@@ -0,0 +1,12 @@
+function calculate()
+{
+    var age = document.getElementById("l1");
+    var bw = document.getElementById("l2");
+    var ltyp = document.getElementById("l3");
+    var lft = document.getElementById("l4");
+    var rps = document.getElementById("l5");
+    //var max = document.getElementById("l6");
+    
+    //document.getElementById("l5").innerHTML = ("haha");
+    document.getElementById("l6").value = ("text");//to set the final value
+}
