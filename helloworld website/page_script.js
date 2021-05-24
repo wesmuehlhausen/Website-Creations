952702274c399d7b3d88721aca9aef9e13e3ae80
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2021-05-23 22:12:28 -0700

    JS finished for max lift calculator

--- helloworld website/page_script.js
+++ helloworld website/page_script.js
@@ -5,18 +5,21 @@
 //  age, weight and reps of a lift to determine your 
 //  theoretical one rep max
 
-//import lifting data from database
-var xlsx = require("xlsx");
-var data = xlsx.readFile("lifting_standards_data.xlsx");
-var wsheet = data.Sheets["standards"];
-var darray = xlsx.utils.sheet_to_json(wsheet);
-//console.log(darray);//print all contents in the database
-var out = darray[3].BW;
-console.log(out); //yields 140 from the data sheet
-console.log(out.toString());
-var x = out + 3
-var tmp = JSON.stringify(darray[3]);
+//DATABASE 
+    //var xlsx = require("xlsx");
+    //var data = xlsx.readFile("lifting_standards_data.xlsx");
+    //var wsheet = data.Sheets["standards"];
+    //var darray = xlsx.utils.sheet_to_json(wsheet);
+    //console.log(darray);//print all contents in the database
+    //var out = darray[3].BW;
+    //console.log(out); //yields 140 from the data sheet
+    //console.log(out.toString());
+    //var x = out + 3
+    //console.log(x);
+    //document.getElementById("strlvl").innerHTML = out;
 
+
+//import lifting data from database
 function calculate() {
     
     
@@ -32,34 +35,12 @@ function calculate() {
     else{//if valid numbers not set
         document.getElementById("l6").value = "Error";//to set the final value
     }
-    
-    document.getElementById("l6").value = tmp;
-    document.getElementById("strlvl").innerHTML = tmp;
-    
-    
-
-    /*
-    if(wgt <= 310 || wgt >= 110){
-        //find where in the range it is and report based on that
-        var adj_wgt = (Math.round(wgt / 10) * 10);
-        var index = ((adj_wgt / 10) - 10);//find the valid index for given weight
-        document.getElementById("strlvl").innerHTML = darray[3].BW;// FIX Get value from darray to work
-    }
-    else{
-        document.getElementById("wgt").value = "Enter weight from 110-310Lbs";
-    }
-    */
-    
-    
-    //document.getElementById("wgt").value = (Math.round(wgt / 10) * 10);//to set the final value
-    //document.getElementById("strlvl").innerHTML = (Math.round(max / 10) * 10) + "%";//to set the final value
-   
-    //console.log(Math.round(wgt / 10) * 10);
 }
 
+//calculation of 1rm given reps and weight
 function epleyFormula(r, w){
-    return w * (1 + ( r / 30 ));
+    if(r == 1)
+        return w;
+    else
+        return w * (1 + ( r / 30 ));
 }
-
-
-//document.getElementById("l5").innerHTML = ("haha");
