9518e510e0673138687b94a934d5500d2c09722e
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2021-05-28 23:42:55 -0700

    starting tab fades

--- wesmuehlhausen.com/page_script.js
+++ wesmuehlhausen.com/page_script.js
@@ -3,15 +3,26 @@ function click_tab(val, rgb){
     var tab = document.getElementById(val);
     // tab.innerHTML = "haha";
 
+    //color all other tabs white
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabs");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.backgroundColor = "white";
+      tabcontent[i].style.color = "black";
     }
 
+
+
+    //set color of selected tab
     tab.style.backgroundColor = rgb;
+    tab.style.color = "white";
     document.body.style.backgroundColor = rgb;
     
-
 }
 
+
+
+
+  
+
+document.getElementById("home_tab").click();
