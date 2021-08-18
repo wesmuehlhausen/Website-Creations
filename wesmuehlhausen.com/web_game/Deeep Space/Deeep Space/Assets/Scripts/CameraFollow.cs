f83240325b310e316dff3f013694ace5583d7f7c
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2021-08-18 16:05:46 -0700

    smoother enemy movement

--- wesmuehlhausen.com/web_game/Deeep Space/Deeep Space/Assets/Scripts/CameraFollow.cs
+++ wesmuehlhausen.com/web_game/Deeep Space/Deeep Space/Assets/Scripts/CameraFollow.cs
@@ -5,6 +5,37 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform myTarget;
+    public GameObject ship;
+
+    void Start()
+    {
+        ship = GameObject.Find("ship_normal");
+        myTarget = ship.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ship == null)
+        {
+            ship = GameObject.Find("ship_normal");
+            myTarget = ship.transform;
+        }
+        if (myTarget != null)
+        {
+            Vector3 targPos = myTarget.position;
+            targPos.z = transform.position.z;
+            transform.position = targPos;
+        }
+    }
+}
+
+
+
+/*
+ public class CameraFollow : MonoBehaviour
+{
+    public Transform myTarget;
 
     // Update is called once per frame
     void Update()
@@ -17,3 +48,4 @@ public class CameraFollow : MonoBehaviour
         }
     }
 }
+ */
--- wesmuehlhausen.com/web_game/Deeep Space/Deeep Space/Assets/Scripts/EnemyMoveForward.cs
+++ wesmuehlhausen.com/web_game/Deeep Space/Deeep Space/Assets/Scripts/EnemyMoveForward.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyMoveForward : MonoBehaviour
 {
-
-    public float movementSpeed = 3f;
-    public GameObject gameObj;
-    public float distance = 0;
+    public float topSpeed = 3f;
+    public float movementSpeed = 0f;
+    public float distance;
+    public float stopAt = 4;
+    public float acceleration = 0.1f;
+    Transform player;
+    GameObject gameObj;
 
     public Sprite[] spriteArray;
     public SpriteRenderer spriteRenderer;
@@ -15,22 +18,47 @@ public class EnemyMoveForward : MonoBehaviour
     void Start()
     {
         spriteRenderer.sprite = spriteArray[0];
+        gameObj = GameObject.Find("ship_normal");
     }
 
     void Update()
     {
+        //Try to find the ship
+        if(gameObj == null)
+            gameObj = GameObject.Find("ship_normal");
+
+
         distance = Vector3.Distance(gameObj.transform.position, transform.position);
-        if (distance > 6.4)
+        if (distance > stopAt)
         {
+            if (movementSpeed > topSpeed)
+            {
+                movementSpeed = topSpeed;
+            }
+            else if (movementSpeed < topSpeed)
+            {
+                movementSpeed += acceleration;
+            }     
             spriteRenderer.sprite = spriteArray[1];
             Vector3 shipPos = transform.position;
             Vector3 shipVelocity = new Vector3(0, movementSpeed * Time.deltaTime, 0);
             shipPos += transform.rotation * shipVelocity;
-            transform.position = shipPos;        
+            transform.position = shipPos;
+
         }
         else
+        {
             spriteRenderer.sprite = spriteArray[0];
+            Vector3 shipPos = transform.position;
+            if (movementSpeed > 0)
+                movementSpeed -= acceleration;
+            else if (movementSpeed < 0)
+                movementSpeed = 0f;
 
+            Vector3 shipVelocity = new Vector3(0, movementSpeed * Time.deltaTime, 0);
+            shipPos += transform.rotation * shipVelocity;
+            transform.position = shipPos;
+        }
     }
 
 }
